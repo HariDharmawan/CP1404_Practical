756a9910d1113c60d4ccd806cebc28b80a0f6894
HariDharmawan <harri_dharmawan@yahoo.com>
2016-04-26 11:36:42 +0800

    fairly done

--- Week5/.idea/ascii_table.py
+++ Week5/.idea/ascii_table.py
@@ -1,14 +1,32 @@
-def get_number(firstInput, SecondInput):
-    while firstInput > secondInput:
-        print("Please enter a valid number!")
-    while s
+LOWER = 10
+UPPER = 100
 
-lower = 10
-upper = 100
 
-firstInput = int(input("Enter a number {}-{}:".format(lower,upper)).strip())
-secondInput = int(input("Enter a number {}-{}:".format(lower,upper)).strip())
+def get_number():
+    while True:
+        low = input("Enter a number({}-{}):".format(LOWER, UPPER)).strip()
+        if low.isdecimal():
+            int1 = int(low)
+            if LOWER <= int1 <= UPPER:
+                break
+            else:
+                print("Please enter a valid number")
+        else:
+            print("Please enter a valid number")
+    int1=int(low)
+    while True:
+        upp = input("Enter a number({}-{}):".format(LOWER, UPPER)).strip()
+        if upp.isdecimal():
+            int2 = int(upp)
+            if int1 <= int2 <= UPPER:
+                break
+            else:
+                print("Please enter a valid number")
+        else:
+            print("Please enter a valid number")
+    int2=int(upp)
+    return int1,int2
 
-for i in range(firstInput,secondInput):
-
-    print("{} {}".format(i, chr(i).strip()))
+low,upp=get_number()
+for i in range(low,upp):
+    print("{} {}".format(i, chr(i)))
