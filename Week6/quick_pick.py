a052adb216b95d51283485e4c768a9e0b992f3a2
HariDharmawan <harri_dharmawan@yahoo.com>
2016-04-26 14:05:35 +0800

    quick pick done

--- Week6/quick_pick.py
+++ Week6/quick_pick.py
@@ -1,3 +1,15 @@
 import random
-user_input = input("How many quick picks?")
+user_input = int(input("How many quick picks?"))
 
+for i in range(user_input):
+    numbers = []
+    for i in range(6):
+        while True:
+            rand_num = random.randint(1,45)
+            if rand_num in numbers:
+                rand_num = random.randint(1,45)
+            else:
+                numbers.append(rand_num)
+                break
+    numbers.sort()
+    print("{:2d}, {:2d}, {:2d}, {:2d}, {:2d}".format(numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5]))
